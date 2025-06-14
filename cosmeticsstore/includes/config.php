65b361a88d1b594173fef3160e034848762524f0
Kevin Ndayizeye <88332689+ndayizeyekevin@users.noreply.github.com>
2025-06-14 23:53:26 +0200

    Update config.php

--- cosmeticsstore/includes/config.php
+++ cosmeticsstore/includes/config.php
@@ -1,10 +1,10 @@
 <?php
 return [
-    'email_username'            => 'mayikissyou2twice@gmail.com',
-    'email_password'            => 'uqyg lzct rlir gpbn',
+    'email_username'            => 'example@gmail.com', //replace with your email
+    'email_password'            => 'xxxx xxxx xxxx xxxx', //replace with your app password
     'logo_url'                  => '../images/logo.png',
-    'support_email'             => 'ndayizeye.kevin@outlook.com',
-    'support_phone'             => '+250 786 591 604',
+    'support_email'             => 'example@outlook.com',
+    'support_phone'             => '+250 786 123 456',
     'phpmailer_src'             => '../assets/phpmailer/src',
     'officialWebsite'           => 'http://127.0.0.1/cosmeticsstore/',
     'location'                  => 'KG 60 Ave',
