4d0481a890a8af2da6104676ea1f512e246cdc67
Kevin NDAYIZEYE <ndayizeyekevin6@gmail.com>
2025-06-14 23:45:46 +0200

    Clean repo to upload fresh code

--- staff/draft.php
+++
@@ -1,8 +0,0 @@
-<?php include './includes/header.php'; ?>
-
-<!-- Page Content Start -->
-<h1>Welcome to the Dashboard</h1>
-<p>This is the main content area. Place your page content here.</p>
-<!-- Page Content End -->
-
-<?php include './includes/footer.php'; ?>
